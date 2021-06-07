c920a082bcc1db3e367bf3267744bb96010169ef
CosimoTommasi <84326051+CosimoTommasi@users.noreply.github.com>
2021-06-07 16:50:29 +0200

    Update MarkImage.m

--- MarkImage.m
+++ MarkImage.m
@@ -1,14 +1,37 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Script per aggiungere un marker di lunghezza data a una o piu' immagini
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 close all
 clear all
 
+%% Parametri da regolare
+um4pix = 0.43;  % micrometri per pixel nell'immagine
+marker = 100;   % lunghezza del marker, in micrometri
+
+%% Ciclo di stampa
 flag = true;
 while flag
     [fnm, canceled] = imgetfile();
+    if canceled
+        disp('USER CANCELED');
+        return
+    end
     img = imread(fnm);
+    % Calcolo della posizione del marker
+    [H,W,C] = size(img);
+    y = round(0.852*H);
+    h = round(0.0185*H);
+    w = round(marker/um4pix);
+    x = round(W*0.9323 - w);
+    
+    % Applicazione del marker
     f=figure();
     imshow(img);
-    rectangle('Position',[1550, 920, 240, 20],'FaceColor','r','EdgeColor','r');
-    text(1570, 880, '100 um', 'FontSize', 24, 'Color','r','FontName','Calibri');
+    rectangle('Position',[x, y, w, h],'FaceColor','r','EdgeColor','r');
+    text(x+h, y-2*h, [num2str(marker),' um'], 'FontSize', 24, 'Color','r','FontName','Calibri');
     
     imwrite(getframe(f).cdata,strcat(fnm,'_MARKED.bmp'));
     
