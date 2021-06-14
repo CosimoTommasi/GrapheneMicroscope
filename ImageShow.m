55929fdb6ba8564a52f848e346987cf8c20c9116
CosimoTommasi <84326051+CosimoTommasi@users.noreply.github.com>
2021-06-14 19:02:48 +0200

    Update ImageShow.m

--- ImageShow.m
+++ ImageShow.m
@@ -5,6 +5,7 @@
 % Navigation commands:
 %   →/←: change picture
 %   ↑/↓: change color (RGB, R only, G only)
+%   Space: select image to jump to
 %   Esc: close image and exit
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -83,6 +84,23 @@ function eKeyPress(sou,eve)
                 shown.CData = img{c};
                 title(strcat(num_image(i),color{c}));
             end
+        case 'space'
+            answer = inputdlg('Enter image number:','Jump to image',[1 50]);
+            if isempty(answer)
+                return
+            end
+            num = str2num(answer{1});
+            if num > length(num_image)
+                errordlg('Invalid input number','Error');
+                return
+            end
+            i = num;
+            c = 1;
+            img{1} = imread(fn{i});
+            img{2} = repmat(img{1}(:,:,1), [1,1,3]);
+            img{3} = repmat(img{1}(:,:,2), [1,1,3]);
+            shown.CData = img{c};
+            title(strcat(num_image(i),color{c}));
         case 'escape'
             close
     end
